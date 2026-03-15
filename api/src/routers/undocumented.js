2f263c0c2a55434e527cce29d396694eb0bae596
oleksiybondar <bondaroleksiyandriyovich@gmail.com>
2026-03-15 21:28:29 +0100

    remove undocumented example

--- api/src/routers/undocumented.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-const unDocumentedDemo = express.Router();
-
-unDocumentedDemo.get("/", (_req, res) => res.json({message: "Hello I'm undocumented endpoint!"}))
-
-export default unDocumentedDemo;
