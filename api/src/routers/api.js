332550993ecf0496fe9a7a28646379c45137d06f
oleksiybondar <bondaroleksiyandriyovich@gmail.com>
2026-03-15 21:33:44 +0100

    update router according to latest changes

--- api/src/routers/api.js
+++ api/src/routers/api.js
@@ -1,10 +1,8 @@
 import express from "express";
 import eventsRouter from "#routers/events.js";
-import unDocumentedDemo from "#routers/undocumented.js";
 
 const apiRouter = express.Router();
 
 apiRouter.use("/events", eventsRouter);
-apiRouter.use("/undocumented", unDocumentedDemo);
 
 export default apiRouter;
